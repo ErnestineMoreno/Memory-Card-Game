ac846556026caca6ed9c6c08b760c0a4160402b8
vadertin1104 <ernestinemoreno4@gmail.com>
2020-03-02 00:08:28 -0800

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+let cardOne = "Queen of Diamonds";
+let cardTwo = "Queen of Hearts";
+let cardThree = "King of Diamonds";
+let cardFour = "King of Hearts";
+console.log("User Flipped " + cardOne);
+
+
